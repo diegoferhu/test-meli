b297b0cb5143a40ae9a689af730a69bbe5abe038
diegoferhu <diegoferhu@gmail.com>
2022-05-03 16:11:54 -0500

    :lipstick: Styled header component

--- src/shared/components/Search.js
+++ src/shared/components/Search.js
@@ -6,23 +6,32 @@ const Search = () => {
   const navigate = useNavigate();
 
   const handlerSearch = (e) => {
+    setSearch(e.target.value);
+
     if (e.key === "Enter") {
       e.preventDefault();
-      setSearch(e.target.value);
       navigate(`/items?search=${e.target.value}`);
     }
   };
+
+  const handlerClickSearch = (e) => {
+    e.preventDefault();
+    navigate(`/items?search=${search}`);
+  };
+
   return (
-    <div>
-      <form>
-        <input
-          name="search"
-          type="text"
-          placeholder="search"
-          onKeyDown={handlerSearch}
-        />
-      </form>
-    </div>
+    <form className="search">
+      <input
+        className="search__input"
+        name="search"
+        type="text"
+        placeholder="Buscar productos, marcas y más"
+        onKeyDown={handlerSearch}
+      />
+      <button className="search__button" onClick={handlerClickSearch}>
+        <i className="fa fa-search"></i>
+      </button>
+    </form>
   );
 };
 
