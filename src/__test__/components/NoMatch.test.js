08603109101fad25d4e2be0049e824e335cf62e6
diegoferhu <diegoferhu@gmail.com>
2022-05-03 22:28:18 -0500

    :recycle: Refactor not found test

--- src/__test__/components/NoMatch.test.js
+++ src/__test__/components/NoMatch.test.js
@@ -1,12 +1,12 @@
 import "jsdom-global/register";
 import React from "react";
-import Enzyme from "enzyme";
+import { mount } from "enzyme";
 import NoMatch from "../../shared/components/NoMatch";
 
 describe("NoMatch", () => {
   test("should render NoMatch component", () => {
-    const wrapper = Enzyme.mount(<NoMatch />);
-    expect(wrapper.find("h2").text()).toBe("404 Not Found");
-    expect(wrapper).toMatchSnapshot();
+    const wrapperNoMatch = mount(<NoMatch />);
+    expect(wrapperNoMatch.find("h2").text()).toBe("404 Not Found");
+    expect(wrapperNoMatch).toMatchSnapshot();
   });
 });
